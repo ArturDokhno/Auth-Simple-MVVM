ec1ed9a6feec5351431d8e497d8ec3825123c851
ArturDokhno <artur.dohno@icloud.com>
2022-06-01 17:59:33 +0500

    Binding в ViewController

--- Auth Simple MVVM/Views/ViewController.swift
+++ Auth Simple MVVM/Views/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var viewModel = ViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        bindViewModel()
     }
 
-
+    func bindViewModel() {
+        viewModel.statusText.bind({ (statusText) in
+            DispatchQueue.main.async {
+                print(statusText)
+            }
+        })
+    }
 }
 
